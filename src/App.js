4723ba5a03346eee33447a4ca4f38e7e67d605a0
gallerypcf <jgallerypcf@gmail.com>
2021-01-04 18:14:46 +0530

    header part setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Akshay lets build slack clone :rocket</h1>
+
+      {/* Header */}
+      <Header />
+      {/* Sidebar */}
+      {/* React-Router -> chat scree 8 */}
     </div>
   );
 }
